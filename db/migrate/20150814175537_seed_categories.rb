7bc5eaa0835b87c828d2d2625797a254941423ad
Ryan Oglesby <ryan.oglesby08@gmail.com>
2015-09-08 10:48:14 -0400

    Category seed migrate uses multiple inserts to appease lower versions of sqlite3

--- db/migrate/20150814175537_seed_categories.rb
+++ db/migrate/20150814175537_seed_categories.rb
@@ -1,13 +1,12 @@
 class SeedCategories < ActiveRecord::Migration
   def up
     execute <<-SQL
-      INSERT INTO categories (name, created_at, updated_at) VALUES
-                             ('Business as Usual', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-                             ('Story Movements', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-                             ('Open Questions', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-                             ('Blockers', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-                             ('Action Items', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-                             ('Other', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+      INSERT INTO categories (name, created_at, updated_at) VALUES ('Business as Usual', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+      INSERT INTO categories (name, created_at, updated_at) VALUES ('Story Movements', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+      INSERT INTO categories (name, created_at, updated_at) VALUES ('Open Questions', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+      INSERT INTO categories (name, created_at, updated_at) VALUES ('Blockers', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+      INSERT INTO categories (name, created_at, updated_at) VALUES ('Action Items', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+      INSERT INTO categories (name, created_at, updated_at) VALUES ('Other', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
     SQL
   end
 
