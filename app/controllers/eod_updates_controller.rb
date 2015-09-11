ca9b22c8b58dd97aa721decae217b743631f5677
Ryan Oglesby <ryan.oglesby08@gmail.com>
2015-09-11 18:42:05 -0500

    Preserve author and content when the submission fails

--- app/controllers/eod_updates_controller.rb
+++ app/controllers/eod_updates_controller.rb
@@ -1,29 +1,22 @@
 class EodUpdatesController < ApplicationController
   def new
     @categories = Category.includes(:entries)
+    @eod_update = EodUpdate.new
   end
 
   def create
-    eod_update = params[:eod_update]
+    eod_update = EodUpdate.build(params[:eod_update])
 
-    Entry.transaction do
-      categories_with_content(eod_update[:content]).each do |category_id, content|
-        entry = Entry.new(author: eod_update[:author], content: content, category_id: category_id)
+    unless Entry.save_eod_update(eod_update)
+      flash.now[:error] = "Oops! Something's wrong."
 
-        unless entry.save
-          flash.now[:error] = "Oops! Something's wrong."
-          @categories = Category.includes(:entries)
-          render :new and return
-        end
+      @categories = Category.includes(:entries)
+      @eod_update = eod_update
 
-      end
+      render :new and return
     end
 
     flash[:info] = 'Thanks! Go home in peace.'
     redirect_to root_path
-  end
-
-  def categories_with_content(content_by_category)
-    content_by_category.select { |_, content| content.present? }
   end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,4 +2,16 @@ class Entry < ActiveRecord::Base
   validates :author, presence: true
 
   scope :undelivered, -> { where(delivered: false) }
+
+  def self.save_eod_update(eod_update)
+    Entry.transaction do
+      eod_update.content_by_category.each do |category_id, content|
+        entry = Entry.new(author: eod_update.author, content: content, category_id: category_id)
+        return false unless entry.save
+      end
+
+    end
+
+    return true
+  end
 end
