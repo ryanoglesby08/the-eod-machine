aa6d2812405a1b0bb692fd8311af2ed1713a9fb9
Ryan Oglesby <ryan.oglesby08@gmail.com>
2018-07-26 15:11:14 -0500

    add mongo to the api

--- api/index.js
+++ api/index.js
@@ -5,13 +5,35 @@ const bodyParser = require('body-parser')
 
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
 
-const schema = require('./schema')
-
-const PORT = 4000
-
+const mongoose = require('mongoose')
 const app = express()
 
-app.use('/api/graphql', cors(), bodyParser.json(), graphqlExpress({ schema }))
-app.get('/api/graphiql', graphiqlExpress({ endpointURL: '/api/graphql' })) // if you want GraphiQL enabled
+const schema = require('./schema')
 
-app.listen(PORT)
+const init = async () =>
+  await mongoose.connect('mongodb://localhost/eodmachine') // TODO this needs to be parameterized to work with docker
+
+init()
+
+const entrySchema = new mongoose.Schema({
+  category: String,
+  content: String,
+})
+
+const Entry = mongoose.model('Entry', entrySchema)
+
+app.use('/api/graphql', cors(), bodyParser.json(), graphqlExpress({ schema }))
+app.get('/api/graphiql', graphiqlExpress({ endpointURL: '/api/graphql' }))
+
+app.get('/api/test', async (req, res) => {
+  const entry = new Entry({
+    category: 'Business as Usual',
+    content: 'This is my update, it rocks',
+  })
+  const savedEntry = await entry.save()
+
+  res.send(savedEntry)
+})
+
+const PORT = 4000
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`))
