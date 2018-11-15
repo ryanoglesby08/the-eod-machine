4804b3bca9e2dba4893d748e7579f73f2df26ddd
Ryan Oglesby <ryan.oglesby08@gmail.com>
2018-11-14 22:03:47 -0600

    Revert "start validating queries against the schema"
    Abandon this because I'm tired of fighting babel across projects
    
    This reverts commit 0bc697b7313f99373a2e96ea1cdb5c2cd442f2c3.

--- api/jest.config.js
+++ api/jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  rootDir: 'src',
-  roots: ['<rootDir>', '<rootDir>/../../__test-utils__/'],
-  globalSetup: '<rootDir>/__test-utils__/globalSetup.js', // Copied from https://github.com/vladgolubev/jest-mongodb
-  globalTeardown: '<rootDir>/__test-utils__/globalTeardown.js', // Copied from https://github.com/vladgolubev/jest-mongodb
-  testEnvironment: '<rootDir>/__test-utils__/mongoEnvironment.js', // Copied from https://github.com/vladgolubev/jest-mongodb
+  roots: ['<rootDir>', '<rootDir>/../__test-utils__/'],
+  globalSetup: './src/__test-utils__/globalSetup.js', // Copied from https://github.com/vladgolubev/jest-mongodb
+  globalTeardown: './src/__test-utils__/globalTeardown.js', // Copied from https://github.com/vladgolubev/jest-mongodb
+  testEnvironment: './src/__test-utils__/mongoEnvironment.js', // Copied from https://github.com/vladgolubev/jest-mongodb
 }
