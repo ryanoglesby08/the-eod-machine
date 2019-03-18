21e9b7768e92d9133a80331d1674ce8e28322944
Ryan Oglesby <ryan.oglesby08@gmail.com>
2019-03-17 20:47:13 -0500

    ui: add locations and eod times to main teams list

--- ui/src/Teams/Teams/Teams.js
+++ ui/src/Teams/Teams/Teams.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 
-import { Heading } from 'rebass/emotion'
+import { Heading, Flex } from 'rebass/emotion'
 
 import TabularList from '../../ui-components/TabularList/TabularList'
 
@@ -14,9 +14,16 @@ export const GET_TEAMS = gql`
     teams {
       _id
       name
+      locations {
+        name
+        eodTime
+      }
     }
   }
 `
+
+const locationsInfo = locations =>
+  locations.map(({ name, eodTime }) => `${name}: ${eodTime}`).join(' | ')
 
 const Teams = ({ match }) => {
   const teamsUrl = match.path
@@ -32,10 +39,11 @@ const Teams = ({ match }) => {
           <div>
             <Heading>Manage teams</Heading>
             <TabularList>
-              {teams.map(({ _id, name }) => (
-                <Link to={`${teamsUrl}/${_id}/edit`} key={_id}>
-                  {name}
-                </Link>
+              {teams.map(({ _id, name, locations }) => (
+                <Flex justifyContent="space-between" key={_id}>
+                  <Link to={`${teamsUrl}/${_id}/edit`}>{name}</Link>
+                  {locationsInfo(locations)}
+                </Flex>
               ))}
             </TabularList>
 
